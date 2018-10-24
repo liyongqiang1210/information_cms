50ee2b44e4e5dd114a0bb336957d8993cfcb8dcc
liyongqiang1210 <1240965061@qq.com>
2018-10-24 08:59:15 +0800

    ..

--- src/main/java/com/maven/realm/MyRealm.java
+++ src/main/java/com/maven/realm/MyRealm.java
@@ -57,7 +57,6 @@ public class MyRealm extends AuthorizingRealm {
 	/**
 	 * 登录验证
 	 */
-	@SuppressWarnings("unlikely-arg-type")
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
 
